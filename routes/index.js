2a85290fb5cebed17889a813b98a32390b22c27b
YhumServant <geojolee@gamil.com>
2023-01-15 00:08:07 +0300

    first commit

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+const Router = require('express')
+const router = new Router()
+const customer_reviewsRouter = require('./customer_reviewsRouter')
+const brandsRouter = require('./brandsRouter')
+const modelsRouter = require('./modelsRouter')
+const detailsRouter = require('./detailsRouter')
+const productsRouter = require('./productsRouter')
+const models_infoRouter = require('./models_infoRouter')
+const mailRouter = require('./mailRouter')
+
+
+
+router.use('/brands', brandsRouter)
+router.use('/models', modelsRouter)
+router.use('/details', detailsRouter)
+router.use('/customer_reviews', customer_reviewsRouter)
+router.use('/products', productsRouter)
+router.use('/models_info', models_infoRouter)
+router.use('/mail', mailRouter)
+
+module.exports = router
